176af88708a7608945a1ab09e65c688f0973a26d
hunafazaky <hunafa.zaky@gmail.com>
2023-01-22 23:21:01 +0700

    update

--- components/Dialog.vue
+++ components/Dialog.vue
@@ -1,49 +1,50 @@
 <template>
-  <div class="text-center">
-    <v-dialog v-model="dialog" width="500">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
-          Click Me
+  <v-dialog
+    v-model="dialog"
+    persistent
+    max-width="290"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn 
+        class="ma-2 px-4" 
+        color="success"
+        v-bind="attrs"
+        v-on="on"
+      >
+        Unggah
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title class="overline">
+        Unggah tulisan ini?
+      </v-card-title>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="success"
+          text
+          @click="dialog = false"
+        >
+          Ya
         </v-btn>
-      </template>
-
-      <v-card>
-        <v-card-title class="text-h5 grey lighten-2">
-          Privacy Policy
-        </v-card-title>
-
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="dialog = false"> I accept </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </div>
+        <v-btn
+          color="error"
+          text
+          @click="dialog = false"
+        >
+          Tidak
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
-export default {
-  name: 'Dialog',
-  props: {
-    dialog: {
-      type: Boolean,
-      default: false,
+  export default {
+    data () {
+      return {
+        dialog: false,
+      }
     },
-  },
-  data() {
-    return {}
-  },
-}
+  }
 </script>
